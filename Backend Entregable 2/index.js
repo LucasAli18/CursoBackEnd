6ffdbd2864b8da4301f82663774c2325b1d76f8d
LucasAli18 <104177558+LucasAli18@users.noreply.github.com>
2023-03-01 21:15:49 -0300

    Desafio 2

---
+++ Backend Entregable 2/index.js
@@ -0,0 +1,14 @@
+const ProductManager = require("./ProductManager");
+const products = new ProductManager("products.json");
+
+/* products.addProduct({
+  title: "prueba",
+  description: "prueba2",
+  price: 120,
+  thumbnail: "prueba.jpg",
+  stock: 12,
+}); */
+//products.getAll();
+//products.updateProduct(1, { title: "UPDATE" });
+//products.getAll()
+//products.deleteById(1);
